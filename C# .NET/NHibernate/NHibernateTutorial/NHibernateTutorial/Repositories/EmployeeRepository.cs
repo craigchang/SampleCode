50f3abf6741319fe650ac93d124e11d1dbab2753
Craig Chang <craigchang@gmail.com>
2015-06-01 00:01:00 -0700

    Add one-to-one mapping sample

---
+++ C# .NET/NHibernate/NHibernateTutorial/NHibernateTutorial/Repositories/EmployeeRepository.cs
@@ -0,0 +1,34 @@
+﻿using NHibernate;
+using NHibernateTutorial.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateTutorial.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public void Add(Employee employee)
+        {
+
+        }
+        public void Update(Employee employee)
+        {
+
+        }
+        public void Remove(Employee employee)
+        {
+
+        }
+        public Employee GetById(Guid employeeId)
+        {
+            return null;
+        }
+        public Employee GetByName(string name)
+        {
+            return null;
+        }
+    }
+}
